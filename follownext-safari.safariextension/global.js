658c16e95ee04ee1fe8de39eb2e7cd07b0693f90
Gavin Gilmour <gavin.gilmour@gmail.com>
2011-02-19 17:14:17 +0000

    refactor'd toolbar buttons into global.js

--- follownext-safari.safariextension/global.js
+++ follownext-safari.safariextension/global.js
@@ -6,14 +6,21 @@ var FN = (function() {
             unsecure: ['nextpattern', 'prevpattern']
         },
 
+        toolbarMap : {
+            'follow-next': 'next',
+            'follow-previous': 'previous'
+        },
+
         init: function() {
 
+            safari.application.addEventListener('validate', this.validate, false);
+            safari.application.addEventListener('command',  this.command, false);
             safari.application.addEventListener('message', function(msg) {
                 FN[msg.name](msg.message, msg.target);
             }, false);
 
-            safari.extension.settings.addEventListener("change", this.saveSettingsToLocalStorage, false);
-            safari.extension.secureSettings.addEventListener("change", this.saveSettingsToLocalStorage, false);
+            safari.extension.settings.addEventListener('change', this.saveSettingsToLocalStorage, false);
+            safari.extension.secureSettings.addEventListener('change', this.saveSettingsToLocalStorage, false);
 
             this.retrieveSettingsFromLocalStorage();
         },
@@ -47,6 +54,20 @@ var FN = (function() {
                 'previous' : safari.extension.settings.getItem('prevpattern'),
                 'next'     : safari.extension.settings.getItem('nextpattern')
             });
+        },
+
+        validate: function(e) {
+            if (!FN.toolbarMap.hasOwnProperty(e.command)) {
+                return;
+            }
+            // Disable the target if the no links are found and this button is even visible
+            // e.target.disabled = /* some logic here to count found links */;
+        },
+
+        command: function(e) {
+            if (FN.toolbarMap.hasOwnProperty(e.command)) {
+                safari.application.activeBrowserWindow.activeTab.page.dispatchMessage('findLink', FN.toolbarMap[e.command]);
+            }
         }
     };
 })();
